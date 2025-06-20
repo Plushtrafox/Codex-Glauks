82bb626ea96f3712b470ac0415ffed6ed6cd80d2
christopher <christopherhayling9@gmail.com>
2025-06-20 16:52:11 -0500

    GAME OVER 2

---
+++ Assets/_Scripts/Game Over.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameOver : MonoBehaviour
+{
+    public void reiniciar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
+    public void MenuInicial(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
+    }
+
+    public void Salir()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+        
+    }
+}
+
+
+
+
+   
+ 
