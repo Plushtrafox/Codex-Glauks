c8a5c54ae560cf65136b554f018b2f509b1ca348
salon 5 <pedropabloramirezarenas80@gmail.com>
2025-07-04 14:59:00 -0500

    pasaron cosas

--- Assets/_Scripts/Jugador/Attacks/Shoot.cs
+++ Assets/_Scripts/Jugador/Attacks/Shoot.cs
@@ -2,18 +2,24 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-    [SerializeField] public GameObject LongBullet; // Prefab de la bala
-    public Transform SpawnPoint; // Punto de aparición de la bala
+    [SerializeField] private ControllesSOSCript controles;
+    [SerializeField] private Rigidbody bulletPrefab;
+    [SerializeField] private Transform spawnPoint; // Punto de origen del proyectil
+    [SerializeField] private float speed = 20f; // Velocidad del proyectil
+    [SerializeField] private float shootTime = 0.5f; // Tiempo entre disparos
+    private float shootStart; // Referencia al script de controles
 
-    [SerializeField] public float bulletSpeed = 20f; // Velocidad de la bala
-    [SerializeField] public float shotRate = 0.5f; // Tiempo entre disparos
-
-   // private float nextShotTime = 0f; // Tiempo del próximo disparo
-    // Update is called once per frame
-
-    void Update()
+    private void Update()
     {
-        
-    }
+        if (Input.GetButtonDown("Fire2") && Time.time >= shootStart + shootTime)
+        {
+            shootStart = Time.time + shootTime; // Actualiza el tiempo del último disparo
+            Rigidbody  bulletPrefabInstanciate; // Variable para almacenar la instancia del proyectil
 
+            bulletPrefabInstanciate = Instantiate(bulletPrefab, spawnPoint.position, Quaternion.identity) as Rigidbody; // Crea el proyectil
+
+            bulletPrefabInstanciate.AddForce(spawnPoint.forward *100* speed); // Aplica fuerza al proyectil
+        }
+    }
 }
+
