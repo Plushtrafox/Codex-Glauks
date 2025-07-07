91d9ca0c16999676e131c2ed34b6e018f4f8b280
christopher <christopherhayling9@gmail.com>
2025-07-07 16:39:29 -0500

    mira de mouse

--- .github/copilot-instructions.md
+++ .github/copilot-instructions.md
@@ -2,13 +2,13 @@
 - Project name: Hairless-Journey
 - Unity version: Unity 6000.0.44f1
 - Active scene:
-  - Name: EjemploEscenario
+  - Name: EscenarioChristopher
   - Tags:
     - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController, Enemy, Muro
   - Layers:
     - Default, TransparentFX, Ignore Raycast, Player, Water, UI, Enemigo
 - Active game object:
-  - Name: ===GameOver===
-  - Tag: Untagged
-  - Layer: Default
+  - Name: Player(base) 1
+  - Tag: Player
+  - Layer: Player
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/_Scripts/RayosX/RayosXScript.cs
+++ Assets/_Scripts/RayosX/RayosXScript.cs
@@ -2,7 +2,36 @@ using UnityEngine;
 
 public class RayosXScript : MonoBehaviour
 {
+    public static int idPosicion = Shader.PropertyToID("PosicionJugador");
+    public static int idSize = Shader.PropertyToID("Size");
+
+
+
     [SerializeField] private Material materialRayosXPared;
     [SerializeField] private Camera camara;
+    [SerializeField] private LayerMask layerMaskRayosX;
+    [SerializeField] private float distanciaRayosX = 100f;
+
+
+    void Update()
+    {
+        Vector3 direccion = camara.transform.position - transform.position;
+        var rayo = new Ray(transform.position, direccion.normalized);
+        var vista = camara.WorldToViewportPoint(transform.position);
+
+        if (Physics.Raycast(rayo, out RaycastHit hit, distanciaRayosX, layerMaskRayosX))
+        {
+            materialRayosXPared.SetFloat(idSize, 1);
+        }
+        else
+        {
+            materialRayosXPared.SetFloat(idSize, 0);
+        }
+
+
+
+
+        materialRayosXPared.SetVector(idPosicion, direccion);
+    }
 
 }
