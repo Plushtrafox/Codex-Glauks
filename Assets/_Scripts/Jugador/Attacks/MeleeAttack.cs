67a490dff6b18981afe3ab7078a30e80428f8e8f
Christopher Hayling <christopherhayling9@gmail.com>
2025-07-14 19:07:33 -0500

    cambio camara

--- Assets/_Scripts/Jugador/Attacks/MeleeAttack.cs
+++ Assets/_Scripts/Jugador/Attacks/MeleeAttack.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class MeleeAttack : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private ControllesSOSCript controles;
     [SerializeField] private Animator animator;
-    [SerializeField] private GameObject ataqueCortoAlcanceCollider;
     AnimatorStateInfo animatorStateInfo; // Variable para almacenar el estado actual del animador
     [SerializeField] private PlumaManager scriptPlumaAtaque; // Referencia al script de la cámara para invocar eventos de sacudida de cámara
+    [SerializeField] private AnimatorBrain animatorBrain; // Referencia al script AnimatorBrain para manejar las animaciones del jugador
 
 
 
@@ -30,11 +31,7 @@ public class MeleeAttack : MonoBehaviour
         scriptPlumaAtaque.EventoReactivarAtaque += ReactivarBoolAtaque; // Suscribirse al evento de reactivación del ataque
     }
 
-    private void Start()
-    {
-        animator.CrossFade("ArmaBase", 0.5f);//nombre incorrecto pero al cambiar el nombre de la animacion no se cambia el nombre de la referencia NO SE PORQUE XD
 
-    }
     private void OnDisable()
     {
         controles.EventoAtaqueEmpieza -= Hit;
@@ -48,7 +45,10 @@ public class MeleeAttack : MonoBehaviour
 
         if (!estaAtacando)
         {
-            animator.CrossFade("AtaqueLigero", 0.08f, 0,0f);//nombre incorrecto pero al cambiar el nombre de la animacion no se cambia el nombre de la referencia NO SE PORQUE XD
+            animatorBrain.ReproducirAnimacion(AnimacionesJugador.JugadorAtaqueCortoAlcanceAnimacion1, 1, true,true);
+            animatorBrain.ReproducirAnimacion(AnimacionesJugador.JugadorAtaqueCortoAlcanceAnimacion1, 0, true, true);
+
+            animator.CrossFade("JugadorAtaqueCortoAlcanceAnimacion1", 0.02f, 0,0f, 0);//nombre incorrecto pero al cambiar el nombre de la animacion no se cambia el nombre de la referencia NO SE PORQUE XD
             animator.Update(0f); // Asegura que el animador esté actualizado antes de verificar el estado
             estaAtacando = true; // Marca que se está atacando para evitar múltiples ataques simultáneos
         }
@@ -60,8 +60,7 @@ public class MeleeAttack : MonoBehaviour
     public void ReactivarBoolAtaque()
     {
         estaAtacando = false; // Marca que ya no se está atacando
-        animator.CrossFade("ArmaBase", 0.2f,0,0f);//nombre incorrecto pero al cambiar el nombre de la animacion no se cambia el nombre de la referencia NO SE PORQUE XD
-        animator.Update(0f);
+
     }
 
 
